9ea3c8f2946db5eacc56ab4742d2ff82c1e7557e
Rob Kent <rob@gulon.co.uk>
2016-07-13 17:46:41 +0100

    Tidying up and defining more methods.

--- RPiQTRSensors-python.cpp
+++ RPiQTRSensors-python.cpp
@@ -6,40 +6,47 @@ using namespace boost::python;
 
 class WrapperFuncs{
 public:
-	static boost::shared_ptr<QTRSensorsRC> init(
-		 	list _pins,
-                        unsigned int timeout,
-		 	unsigned char emitterPin
-		 )
-	{
-		unsigned char __pins[QTRSensors::MAX_SENSORS];
-		for(int i=0; i<len(_pins); i++)
-                    __pins[i]=extract<int>(_pins[i]);
-		return boost::shared_ptr<QTRSensorsRC>( new QTRSensorsRC(__pins, (unsigned char)len(_pins), timeout, emitterPin) );
-	}
+    static boost::shared_ptr<QTRSensorsRC> init(
+        list _pins, unsigned int timeout, unsigned char emitterPin){
+        unsigned char __pins[QTRSensors::MAX_SENSORS];
+        for(int i=0; i<len(_pins); i++)
+            __pins[i]=extract<int>(_pins[i]);
+        return boost::shared_ptr<QTRSensorsRC>(
+            new QTRSensorsRC(__pins, (unsigned char)len(_pins), timeout, emitterPin) 
+        );
+    }
 
-	static unsigned int readLine(QTRSensorsRC &qtrrc, list sensorValues){
-		unsigned int _sv[QTRSensors::MAX_SENSORS];
-		for(int i=0; i<len(sensorValues); i++)
-			_sv[i]=extract<unsigned int>(sensorValues[i]);
-        	int result=qtrrc.readLine(_sv);
-		for(int i=0; i<len(sensorValues); i++)
-			sensorValues[i]=_sv[i];
-                return result;
-	}
+    static unsigned int readLine(QTRSensorsRC &qtrrc, list sensorValues){
+        unsigned int _sv[QTRSensors::MAX_SENSORS];
+        for(int i=0; i<len(sensorValues); i++)
+            _sv[i]=extract<unsigned int>(sensorValues[i]);
+
+        int result=qtrrc.readLine(_sv);
+
+        for(int i=0; i<len(sensorValues); i++)
+            sensorValues[i]=_sv[i];
+        return result;
+    }
 };
 
-
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(
+    calibrate_overloads, QTRSensorsRC::calibrate, 0, 1
+)
 
 BOOST_PYTHON_MODULE(RPiQTRSensors){
-	//class_<QTRSensorsRC>("QTRSensorsRC", init<unsigned char*, unsigned char, unsigned int, unsigned char>())
-	//	.def("calibrate", &QTRSensorsRC::calibrate)
-	//	.def("readLine", &QTRSensorsRC::readLine)
-	//	;
-	class_<QTRSensorsRC, boost::shared_ptr<QTRSensorsRC> >
-		("QTRSensorsRC", no_init)
-		.def("__init__", make_constructor(WrapperFuncs::init))
-		.def("calibrate", &QTRSensorsRC::calibrate)
-		.def("readLine", &WrapperFuncs::readLine)
-	;
+    class_<QTRSensorsRC, boost::shared_ptr<QTRSensorsRC> >
+        ("QTRSensorsRC", no_init)
+        .def("__init__", make_constructor(WrapperFuncs::init))
+
+        // 'Normal' methods
+        .def("calibrate", &QTRSensorsRC::calibrate, calibrate_overloads())
+        .def("emittersOff", &QTRSensorsRC::emittersOff)
+        .def("emittersOn", &QTRSensorsRC::emittersOn)
+        .def("resetCalibration", &QTRSensorsRC::resetCalibration)
+
+        // Wrapped methods
+        .def("readLine", &WrapperFuncs::readLine)
+        //.def("read")
+        //.def("readCalibrated")
+    ;
 }
