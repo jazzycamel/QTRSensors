5c876a324d6003325474ac5b41b3d8da80a70498
Rob Kent <rob@gulon.co.uk>
2016-07-14 15:22:00 +0100

    Code tidy up

--- QTRSensors.h
+++ QTRSensors.h
@@ -1,26 +1,23 @@
 #ifndef QTRSensors_h
 #define QTRSensors_h
 
-#define QTR_EMITTERS_OFF 0
-#define QTR_EMITTERS_ON 1
-#define QTR_EMITTERS_ON_AND_OFF 2
-
-#define QTR_NO_EMITTER_PIN  255
-
-#define QTR_MAX_SENSORS 16
-
-class QTRSensors
-{
+class QTRSensors {
   public:
+    enum {
+      QTR_EMITTERS_OFF=0
+      QTR_EMITTERS_ON=1
+      QTR_EMITTERS_ON_AND_OFF=2
+      QTR_NO_EMITTER_PIN=255
+      QTR_MAX_SENSORS=16
+    };
 
-
-    void read(unsigned int *sensor_values, unsigned char readMode = QTR_EMITTERS_ON);
+    void read(unsigned int *sensor_values, unsigned char readMode=QTR_EMITTERS_ON);
     void emittersOff();
     void emittersOn();
-    void calibrate(unsigned char readMode = QTR_EMITTERS_ON);
+    void calibrate(unsigned char readMode=QTR_EMITTERS_ON);
     void resetCalibration();
-    void readCalibrated(unsigned int *sensor_values, unsigned char readMode = QTR_EMITTERS_ON);
-    int readLine(unsigned int *sensor_values, unsigned char readMode = QTR_EMITTERS_ON, unsigned char white_line = 0);
+    void readCalibrated(unsigned int *sensor_values, unsigned char readMode=QTR_EMITTERS_ON);
+    int readLine(unsigned int *sensor_values, unsigned char readMode=QTR_EMITTERS_ON, unsigned char white_line=0);
     unsigned int *calibratedMinimumOn;
     unsigned int *calibratedMaximumOn;
     unsigned int *calibratedMinimumOff;
@@ -30,11 +27,7 @@ class QTRSensors
 
   protected:
 
-    QTRSensors()
-    {
-
-    };
-
+    QTRSensors(){};
     void init(unsigned char *pins, unsigned char numSensors, unsigned char emitterPin);
 
     unsigned char *_pins;
@@ -44,8 +37,7 @@ class QTRSensors
     int _lastValue;
 
   private:
-
-    virtual void readPrivate(unsigned int *sensor_values) = 0;
+    virtual void readPrivate(unsigned int *sensor_values)=0;
     void calibrateOnOrOff(unsigned int **calibratedMinimum,
                           unsigned int **calibratedMaximum,
                           unsigned char readMode);
@@ -54,13 +46,12 @@ class QTRSensors
 class QTRSensorsRC : public QTRSensors
 {
   public:
-
     QTRSensorsRC();
     QTRSensorsRC(unsigned char* pins, unsigned char numSensors,
-          unsigned int timeout = 4000, unsigned char emitterPin = 255);
+          unsigned int timeout=4000, unsigned char emitterPin=QTR_NO_EMITTER_PIN);
 
     void init(unsigned char* pins, unsigned char numSensors,
-          unsigned int timeout = 2000, unsigned char emitterPin = QTR_NO_EMITTER_PIN);
+          unsigned int timeout=2000, unsigned char emitterPin=QTR_NO_EMITTER_PIN);
 
   private:
     void readPrivate(unsigned int *sensor_values);
