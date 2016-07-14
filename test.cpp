6bf96207c74dd4a21680de0c5b8f9971cbfcc75c
Rob Kent <rob@gulon.co.uk>
2016-07-14 13:06:01 +0100

    Restructure test.cpp as simple input test

--- test.cpp
+++ test.cpp
@@ -1,20 +1,30 @@
-#include "RPiGPIO.h"
+// #include "RPiGPIO.h"
 #include "RPiTime.h"
+#include "RPi.h"
 
 int main(int argc, char *argv[]){
 	RPiTime t;
-	RPiGPIO io;
-	io.init();
+	
+	INP_GPIO(17);
+	t.delay(1000)
+	while(1){
+		printf("GPIO17: %d\n", GPIO_READ(17));
+		t.delay(1000);
+	}
+
+	//RPiTime t;
+	//RPiGPIO io;
+	//io.init();
 
 	//io.pinMode(17, RPiGPIO::OUT);
 	//io.digitalWrite(17, RPiGPIO::HIGH);
 	//io.digitalWrite(17, RPiGPIO::LOW);
 	
-	io.pinMode(17, RPiGPIO::IN);
-	while(1){
-		printf("GPIO4: %d\n", io.digitalRead(17));
-		t.delay(1000);
-	}
+	// io.pinMode(17, RPiGPIO::IN);
+	// while(1){
+	// 	printf("GPIO4: %d\n", io.digitalRead(17));
+	// 	t.delay(1000);
+	// }
 
 	return 0;
 }
