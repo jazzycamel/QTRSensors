5066476019151a9fdfa913ed4880e08e5443f1b4
Rob Kent <rob@gulon.co.uk>
2016-07-14 12:34:05 +0100

    Testin inputs

--- test.cpp
+++ test.cpp
@@ -1,32 +1,20 @@
-// #include "RPi.h"
 #include "RPiGPIO.h"
 #include "RPiTime.h"
 
-int main(int argc, char *argv[]){
-	// if(map_peripheral(&gpio)==-1){
-	// 	printf("Failed to map the physical GPIO registers into the virtual memory space.\n");
-	// 	return -1;
-	// }
-
-	// INP_GPIO(4);
-	// OUT_GPIO(4);
-
-	// while(1){
-	// 	GPIO_SET=1<<4;
-	// 	sleep(1);
-
-	// 	GPIO_CLR=1<<4;
-	// 	sleep(1);
-	// }
-
+int main(int argc, char *argv[]){}
 	RPiTime t;
-
 	RPiGPIO io;
 	io.init();
-	io.pinMode(17, RPiGPIO::OUT);
+
+	//io.pinMode(17, RPiGPIO::OUT);
 	//io.digitalWrite(17, RPiGPIO::HIGH);
-	io.digitalWrite(17, RPiGPIO::LOW);
-	//io.pinMode(17, RPiGPIO::IN);
-	//printf("GPIO4: %d\n", io.digitalRead(4));
+	//io.digitalWrite(17, RPiGPIO::LOW);
+	
+	io.pinMode(17, RPiGPIO::IN);
+	while(1){
+		printf("GPIO4: %d\n", io.digitalRead(17));
+		t.delay(1000);
+	}
+
 	return 0;
 }
