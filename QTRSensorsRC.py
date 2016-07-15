403cb2ac31b26002b3b1584f347648154706f81f
Rob Kent <rob@gulon.co.uk>
2016-07-15 12:39:11 +0100

    Extending python example

--- QTRSensorsRC.py
+++ QTRSensorsRC.py
@@ -3,22 +3,29 @@ from sys import stdout
 from time import sleep
 from QTRSensors import QTRSensorsRC
 
-qtrrc=QTRSensorsRC([1,2,3,4,5,12,13,6], 2500, 0)
-sleep(.5)
-print("Calibrating",end='')
-for i in range(400):
-    if i%10==0: 
-        print('.',end='')
-        stdout.flush()
-    qtrrc.calibrate()
-print("Finished")
+if __name__=="__main__":
+	qtrrc=QTRSensorsRC([1,2,3,4,5,12,13,6], 2500, 0)
+	sleep(.5)
 
-for v in qtrrc.calibratedMinimumOn():
-    print(v,end='\t')
-print()
+	print("Calibrating",end='')
+	for i in range(400):
+	    if i%10==0: 
+	        print('.',end='')
+	        stdout.flush()
+	    qtrrc.calibrate()
+	print("Finished")
 
-sv=[0]*8
-while(1):
-    pos=qtrrc.readLine(sv)
-    print(sv,pos)
-    sleep(1)
+	print("\nCalibration Data (min/max):")
+	for v in qtrrc.calibratedMinimumOn():
+	    print(v,end='\t')
+	print()
+
+	for v in qtrrc.calibratedMaximumOn():
+	    print(v,end='\t')
+	print()
+
+	sv=[0]*8
+	while(1):
+	    pos=qtrrc.readLine(sv)
+	    print(sv,pos)
+	    sleep(1)
